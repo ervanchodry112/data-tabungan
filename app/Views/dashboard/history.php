b3e8126b43c790308934ee3bef802858e94d3fc2
Ervan Chodry <ervanchodry112@gmail.com>
2022-11-17 00:01:27 +0700

    Tambahan setor

--- app/Views/dashboard/history.php
+++ app/Views/dashboard/history.php
@@ -79,20 +79,27 @@ echo $this->section('content');
 <!-- End #main -->
 
 
-<div class="modal fade" id="setorModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="setorModal" tabindex="-1" aria-labelledby="setorModal" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Setor Tunai</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+            <form action="<?= base_url('transactions/add_saldo') ?>" method="post">
+                <div class="modal-body">
+                    <div class="row mb-3">
+                        <label for="nominal" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" id="nominal">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-success">Setor</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
