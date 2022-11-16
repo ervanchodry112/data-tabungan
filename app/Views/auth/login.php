0c8dccf874d9c9a90b8013be6cc13d75b1f2f87e
Ervan Chodry <ervanchodry112@gmail.com>
2022-11-16 21:03:32 +0700

    Membuat sistem login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -11,51 +11,45 @@ echo $this->section('navbar');
 				<div class="row justify-content-center">
 					<div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 						<div class="d-flex justify-content-center py-4">
-							<a href="<?= base_url('') ?>" class="logo grid text-center">
+							<div class="logo grid text-center">
 								<div class="g-col-6 mb-2">
 									<img src="/assets/img/icon.png" alt="">
 								</div>
 								<div class="g-col-6">
 									<span class="d-none d-lg-block">Save Money</span>
 								</div>
-							</a>
+							</div>
 						</div><!-- End Logo -->
 
 						<div class="card mb-3">
 
 							<div class="card-body ">
 								<div class="pt-4 pb-2">
-									<h5 class="card-title text-center pb-0 fs-4">Login</h5>
+									<h5 class="card-title text-center pb-0 fs-4"><?= lang('Auth.loginTitle') ?></h5>
 									<p class="text-center small">Login to <b>Save Money</b></p>
 								</div>
-
-								<form class="row g-3 needs-validation px-3" novalidate>
+								<?= view('Myth\Auth\Views\_message_block') ?>
+								<form action="<?= url_to('login') ?>" class="row g-3 needs-validation px-3" method="post">
+									<?= csrf_field() ?>
 
 									<div class="col-12">
-										<label for="yourUsername" class="form-label">Your Username</label>
+										<label for="yourUsername" class="form-label">Username</label>
 										<div class="input-group has-validation">
-											<input type="text" name="username" class="form-control" id="yourUsername" required>
-											<div class="invalid-feedback">Please enter your username!</div>
+											<input type="text" name="login" class="form-control <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" id="yourUsername" required>
+											<div class="invalid-feedback"><?= session('errors.login') ?></div>
 										</div>
 									</div>
 
 									<div class="col-12">
-										<label for="yourPassword" class="form-label">Your Password</label>
-										<input type="password" name="password" class="form-control" id="yourPassword" required>
-										<div class="invalid-feedback">Please enter your password!</div>
-									</div>
-
-									<div class="col-12">
-										<div class="form-check">
-											<input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
-											<label class="form-check-label" for="rememberMe">Remember Account?</label>
-										</div>
+										<label for="yourPassword" class="form-label">Password</label>
+										<input type="password" name="password" class="form-control <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" id="yourPassword" required>
+										<div class="invalid-feedback"><?= session('errors.password') ?></div>
 									</div>
 									<div class="col-12">
-										<button class="btn btn-primary w-100" type="submit">Login</button>
+										<button class="btn btn-primary w-100" type="submit"><?= lang('Auth.loginAction') ?></button>
 									</div>
 									<div class="col-12">
-										<p class="small mb-0">Don't have account? <a href="<?= base_url('auth/register') ?>">Create an account</a></p>
+										<p class="small mb-0">Don't have account? <a href="<?= url_to('register') ?>">Create an account</a></p>
 									</div>
 								</form>
 							</div>
