2bd7137905476e8bf4f5d98553339eb93256f74a
aribyusronha <arib.yusronhamdani2078@students.unila.ac.id>
2022-11-17 09:23:33 +0700

    Add Profile

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -7,152 +7,72 @@ echo $this->section('navbar');
 <!-- ======= Header ======= -->
 <header id="header" class="header fixed-top d-flex align-items-center">
 
-    <div class="d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
-            <img src="/assets/img/icon.png" alt="" />
-            <span class="d-none d-lg-block">SaveMoney</span>
-        </a>
-        <i class="bi bi-list toggle-sidebar-btn"></i>
-    </div>
-    <!-- End Logo -->
+	<div class="d-flex align-items-center justify-content-between">
+		<a href="index.html" class="logo d-flex align-items-center">
+			<img src="/assets/img/icon.png" alt="" />
+			<span class="d-none d-lg-block">SaveMoney</span>
+		</a>
+		<i class="bi bi-list toggle-sidebar-btn"></i>
+	</div>
+	<!-- End Logo -->
 
 </header>
 <!-- End Header -->
 
 <!-- ======= Sidebar ======= -->
 <aside id="sidebar" class="sidebar">
-    <ul class="sidebar-nav" id="sidebar-nav">
+	<ul class="sidebar-nav" id="sidebar-nav">
 
-        <?php
-        if (in_groups('nasabah')) {
-        ?>
-            <li class="nav-heading">Main</li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('') ?>">
-                    <i class="bi bi-grid"></i>
-                    <span>Dashboard</span>
-                </a>
-            </li>
-            <!-- End Dashboard Nav -->
+		<?php
+		if (in_groups('nasabah')) {
+		?>
+			<li class="nav-heading">Main</li>
+			<li class="nav-item">
+				<a class="nav-link" href="<?= base_url('') ?>">
+					<i class="bi bi-grid"></i>
+					<span>Dashboard</span>
+				</a>
+			</li>
+			<!-- End Dashboard Nav -->
 
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="<?= base_url('dashboard/history') ?>">
-                    <i class="bi bi-clock-history"></i>
-                    <span>History</span>
-                </a>
-            </li>
-        <?php
-        }
-        ?>
+			<li class="nav-item">
+				<a class="nav-link collapsed" href="<?= base_url('dashboard/history') ?>">
+					<i class="bi bi-clock-history"></i>
+					<span>History</span>
+				</a>
+			</li>
+		<?php
+		}
+		?>
 
-        <?php
-        if (in_groups('admin')) {
-        ?>
-            <li class="nav-heading">Admin</li>
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="<?= base_url('dashboard/users') ?>">
-                    <i class="bi bi-people-fill"></i>
-                    <span>Users</span>
-                </a>
-            </li>
-        <?php
-        }
-        ?>
+		<?php
+		if (in_groups('admin')) {
+		?>
+			<li class="nav-heading">Admin</li>
+			<li class="nav-item">
+				<a class="nav-link collapsed" href="<?= base_url('dashboard/users') ?>">
+					<i class="bi bi-people-fill"></i>
+					<span>Users</span>
+				</a>
+			</li>
+		<?php
+		}
+		?>
 
-        <li class="nav-heading">Account</li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="<?= base_url('/auth/login') ?>">
-                <i class="bi bi-person-fill"></i>
-                <span>Profile</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="<?= base_url('/logout') ?>">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Logout</span>
-            </a>
-        </li>
-        <!-- End Dashboard Nav -->
-        <!-- 
-        <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="components-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
-                <li>
-                    <a href="components-alerts.html">
-                        <i class="bi bi-circle"></i><span>Alerts</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-accordion.html">
-                        <i class="bi bi-circle"></i><span>Accordion</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-badges.html">
-                        <i class="bi bi-circle"></i><span>Badges</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-breadcrumbs.html">
-                        <i class="bi bi-circle"></i><span>Breadcrumbs</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-buttons.html">
-                        <i class="bi bi-circle"></i><span>Buttons</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-cards.html">
-                        <i class="bi bi-circle"></i><span>Cards</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-carousel.html">
-                        <i class="bi bi-circle"></i><span>Carousel</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-list-group.html">
-                        <i class="bi bi-circle"></i><span>List group</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-modal.html">
-                        <i class="bi bi-circle"></i><span>Modal</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-tabs.html">
-                        <i class="bi bi-circle"></i><span>Tabs</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-pagination.html">
-                        <i class="bi bi-circle"></i><span>Pagination</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-progress.html">
-                        <i class="bi bi-circle"></i><span>Progress</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-spinners.html">
-                        <i class="bi bi-circle"></i><span>Spinners</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="components-tooltips.html">
-                        <i class="bi bi-circle"></i><span>Tooltips</span>
-                    </a>
-                </li>
-            </ul>
-        </li> -->
-        <!-- End Components Nav -->
-    </ul>
+		<li class="nav-heading">Account</li>
+		<li class="nav-item">
+			<a class="nav-link collapsed" href="<?= base_url('/dashboard/profile') ?>">
+				<i class="bi bi-person-fill"></i>
+				<span>Profile</span>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link collapsed" href="<?= base_url('/logout') ?>">
+				<i class="bi bi-box-arrow-right"></i>
+				<span>Logout</span>
+			</a>
+		</li>
+	</ul>
 
 </aside>
 <!-- End Sidebar-->
@@ -160,17 +80,17 @@ echo $this->section('navbar');
 <?= $this->renderSection('content') ?>
 
 <!-- ======= Footer ======= -->
-<footer id="footer" class="footer position-fixed bottom-0 start-0 end-0">
-    <div class="copyright">
-        &copy; Copyright <strong><span>SaveMoney</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-    </div>
+<footer id="footer" class="footer position-fixed bottom-0 start-0 end-0 bg-light">
+	<div class="copyright">
+		&copy; Copyright <strong><span>SaveMoney</span></strong>. All Rights Reserved
+	</div>
+	<div class="credits">
+		<!-- All the links in the footer should remain intact. -->
+		<!-- You can delete the links only if you purchased the pro version. -->
+		<!-- Licensing information: https://bootstrapmade.com/license/ -->
+		<!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+		Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+	</div>
 </footer>
 <!-- End Footer -->
 
